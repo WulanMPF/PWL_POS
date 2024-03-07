90eac3db6b02240c30c845d532c0258924f5be87
Wulan Maulidya Putri Firmansyah <wulanmaulidya1604@gmail.com>
2024-03-07 22:57:57 +0700

    js4_p2.4

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -7,19 +7,19 @@
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
-                {{-- <td>ID</td>
+                <td>ID</td>
                 <td>Username</td>
                 <td>Nama</td>
-                <td>ID Level Pengguna</td> --}}
-                <td>Jumlah Pengguna</td>
+                <td>ID Level Pengguna</td>
+                {{-- <td>Jumlah Pengguna</td> --}}
             </tr>
             {{-- @foreach ($data as $d) --}}
             <tr>
-                {{-- <td>{{ $data->user_id }}</td>
+                <td>{{ $data->user_id }}</td>
                 <td>{{ $data->username }}</td>
                 <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td> --}}
-                <td>{{ $data }}</td>
+                <td>{{ $data->level_id }}</td>
+                {{-- <td>{{ $data }}</td> --}}
             </tr>
             {{-- @endforeach --}}
         </table>
