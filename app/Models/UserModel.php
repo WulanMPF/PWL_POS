0e7fa83e80e3c91fb7ffa506e5eda8530d144889
Wulan Maulidya Putri Firmansyah <wulanmaulidya1604@gmail.com>
2024-05-02 19:19:08 +0700

    js11_p

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,14 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class UserModel extends Authenticatable implements JWTSubject
 {
+
     public function getJWTIdentifier()
     {
         return $this->getKey();
@@ -20,17 +20,21 @@ class UserModel extends Authenticatable implements JWTSubject
         return [];
     }
 
-    protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id'; // Mendefinisikan primary key dari tabel yang digunakan
-    /**
-     * The attributes that are mass assignable.
-     * 
-     * @var array
-     */
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
+    protected $table = 'm_user';
+    protected $primaryKey = 'user_id';
 
-    public function level(): BelongsTo
+    protected $fillable = [
+        'username', 'nama', 'password', 'level_id', 'image' //tambahan
+    ];
+
+    public function level()
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($image) => url('/storage/posts/' . $image),
+        );
     }
 }
